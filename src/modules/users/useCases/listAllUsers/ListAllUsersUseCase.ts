4dcdba56b54ec4d4cf02867fe2ebb397196903e0
Leonardo Braz <lhleonardo05@gmail.com>
2021-09-06 00:22:34 -0300

    Listagem de usuários para admins

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -6,10 +6,24 @@ interface IRequest {
 }
 
 class ListAllUsersUseCase {
-  constructor(private usersRepository: IUsersRepository) {}
+  private usersRepository: IUsersRepository;
+
+  constructor(usersRepository: IUsersRepository) {
+    this.usersRepository = usersRepository;
+  }
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const userRequest = this.usersRepository.findById(user_id);
+
+    const isAdmin = userRequest?.admin ?? false;
+
+    if (!isAdmin) {
+      throw new Error("User must be admin");
+    }
+
+    const users = this.usersRepository.list();
+
+    return users;
   }
 }
 
