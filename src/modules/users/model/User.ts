8079fa0187bbd85025dc4784a1f599979b72b2a6
Leonardo Braz <lhleonardo05@gmail.com>
2021-09-05 22:38:17 -0300

    Define modelo de usuário

--- src/modules/users/model/User.ts
+++ src/modules/users/model/User.ts
@@ -1,7 +1,23 @@
-import { v4 as uuidV4 } from "uuid";
+import { v4 as uuid } from "uuid";
 
 class User {
   // Complete aqui
+  id: string;
+  name: string;
+  email: string;
+  admin: boolean;
+
+  created_at: Date;
+  updated_at: Date;
+
+  constructor() {
+    this.id = uuid();
+
+    this.created_at = new Date();
+    this.updated_at = new Date();
+
+    this.admin = false;
+  }
 }
 
 export { User };
